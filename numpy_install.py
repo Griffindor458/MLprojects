af0ae576d7b915b87ff036c4cfb8b5ee3c223bd3
Debapriya Mukherjee <deblinamukherjee2021@gmail.com>
2025-05-02 05:24:56 +0530

    logging and exception

---
+++ numpy_install.py
@@ -0,0 +1,6 @@
+
+import numpy as np 
+
+npnam = np.array([[1,2],[3,4]])
+
+print(npnam)
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT ='.e'
+def get_requirements(file_str:str)->List[str]:
+   '''
+   this function will return the lists of requirements
+
+   '''
+
+   requirements=[]
+   with open(file_path) as file_obj:
+    requirements=file_obj.readlines()
+    requirements=[req.replace("\n") for req in requirements]
+
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+name = 'MLprojects',
+version = '0.0.1',
+author ='Deblina',
+author_email = 'deblinamukherjee2021@gmail.com',
+package = find_packages(),
+install_requires =get_requirements('requirements.txt')
+
+
+)
+
