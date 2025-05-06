795a93e1a7335417baba1e7f77d3400670eb94fb
Debapriya Mukherjee <deblinamukherjee2021@gmail.com>
2025-05-07 02:21:36 +0530

    EDA and Problem statements

--- setup.py
+++ setup.py
@@ -1,31 +1,30 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT ='.e'
-def get_requirements(file_str:str)->List[str]:
-   '''
-   this function will return the lists of requirements
-
-   '''
-
-   requirements=[]
-   with open(file_path) as file_obj:
-    requirements=file_obj.readlines()
-    requirements=[req.replace("\n") for req in requirements]
+HYPEN_E_DOT = '-e .'
 
 
-    if HYPEN_E_DOT in requirements:
-        requirements.remove(HYPEN_E_DOT)
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 
 setup(
-name = 'MLprojects',
-version = '0.0.1',
-author ='Deblina',
-author_email = 'deblinamukherjee2021@gmail.com',
-package = find_packages(),
-install_requires =get_requirements('requirements.txt')
-
+    name='mlproject',
+    version='0.0.1',
+    author='Krish',
+    author_email='krishnaik06@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 
 )
-
